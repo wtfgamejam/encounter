cbabe7c0a2c428b472dafee6eb42f79b87e66d86
Mike Thorpe <mthorpe@unbounce.com>
2015-05-18 09:40:05 -0700

    Semi..

--- www/js/main.js
+++ www/js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
 
   /* SETUP */
   function load_data(file) {
-    var json = null;
-    url = "../sets/json/" + file + ".json";
+    var json = null
+    url = "../sets/json/" + file + ".json"
     $.ajax({
       'async': false,
       'global': false,
       'url': url,
       'dataType': "json",
       'success': function(data) {
-        json = data;
+        json = data
       }
-    });
-    return json;
+    })
+    return json
   }
 
   var player = {
@@ -25,27 +25,27 @@ $(document).ready(function() {
     "loot"    : 0
   }
 
-  var weapons = load_data("weapons");
-  var monsters = load_data("monsters");
-  var magic = load_data("magic");
-  var armor = load_data("armor");
+  var weapons = load_data("weapons")
+  var monsters = load_data("monsters")
+  var magic = load_data("magic")
+  var armor = load_data("armor")
   
   function shuffle(){
     $("#card-deck").each(function(){
-        var divs = $(this).find('.card');
+        var divs = $(this).find('.card')
 
-        for(var i = 0; i < divs.length; i++) $(divs[i]).remove();            
-        var i = divs.length;
-        if ( i == 0 ) return false;
+        for(var i = 0; i < divs.length; i++) $(divs[i]).remove()   
+        var i = divs.length
+        if ( i == 0 ) return false
         while ( --i ) {
-           var j = Math.floor( Math.random() * ( i + 1 ) );
-           var tempi = divs[i];
-           var tempj = divs[j];
-           divs[i] = tempj;
-           divs[j] = tempi;
+           var j = Math.floor( Math.random() * ( i + 1 ) )
+           var tempi = divs[i]
+           var tempj = divs[j]
+           divs[i] = tempj
+           divs[j] = tempi
          }
         for(var i = 0; i < divs.length; i++) $(divs[i]).appendTo(this);
-    });                    
+    })                   
   }
 
   /* PLAYER FUNCTIONS */
@@ -62,36 +62,36 @@ $(document).ready(function() {
   /* UPDATE FUNCTIONS */
 
   function update_player (player) {
-    $('#player-health').html(player.health);
-    $('#player-defense').html(player.defense);
-    $('#player-attack').html(player.attack);
-    $('#player-loot').html(player.loot);
+    $('#player-health').html(player.health)
+    $('#player-defense').html(player.defense)
+    $('#player-attack').html(player.attack)
+    $('#player-loot').html(player.loot)
   }
 
   function update_hints () {
-    var hint_left = "Equip";
-    var hint_right = "Loot";
-    var card_type = $("#card-deck .card").first().attr('card-type');
+    var hint_left = "Equip"
+    var hint_right = "Loot"
+    var card_type = $("#card-deck .card").first().attr('card-type')
 
     switch(card_type) {
       case "monster":
         hint_left = "Fight!";
         hint_right = "Run Away!";
-        break;
+        break
       case "weapon":
-        break;
+        break
       case "armor":
-        break;
+        break
       default:
-        hint_left = "Keep";
+        hint_left = "Keep"
     }
-    $('#hint-left').html(hint_left);
-    $('#hint-right').html(hint_right);
+    $('#hint-left').html(hint_left)
+    $('#hint-right').html(hint_right)
 
   }
 
   function game_over () {
-    window.alert('game');
+    window.alert('game')
   }
 
   /* CARD CONSTRUCTION FUNCTIONS */
@@ -100,168 +100,170 @@ $(document).ready(function() {
     var block = "<div class='stat'>" +
       "<div class='stat-name'>" + name + "</div>" +
       "<div class='stat-value' name='" + name + "'>" + value + "</div>" +
-      "</div>";
-    return block;
+      "</div>"
+    return block
   }
 
   function add_avatar(image) {
     var block = '<div class="avatar"' +
-      ' style="background-image: url(';
+      ' style="background-image: url('
     // 'assets/monster/';
-    block = block + image;
-    block = block + ')"></div>';
-    return block;
+    block = block + image
+    block = block + ')"></div>'
+    return block
   }
 
   function add_card_monster(monster) {
     // Add a card to the deck named after the monster
     $('#card-deck').append('<div class="card"' +
       ' id=' + monster.name +
-      ' card-type="monster">');
+      ' card-type="monster">')
 
-    var monster_card = $('#' + monster.name);
+    var monster_card = $('#' + monster.name)
 
     // Add background
     console.log(monster.background);
     if (monster.background) {
-      monster_card.css('background-image',"url('"+monster.background+"')");
+      monster_card.css('background-image',"url('"+monster.background+"')")
     }
 
     // Top Label
     monster_card.append(
       '<div class="card-name">' +
       monster.name +
-      '</div>');
+      '</div>')
 
     // Image
     monster_card.append(
-      add_avatar(monster.image));
+      add_avatar(monster.image))
 
     // Stats
     monster_card.append(
-      '<div class="card-stats">');
+      '<div class="card-stats">')
 
     monster_card.find(".card-stats").append(
       add_stat("health", monster.health),
       add_stat("attack", monster.attack),
       add_stat("defense", monster.defense)
-    );
+    )
 
     // Description
     monster_card.append(
       '<div class="card-description">'+
       monster.description+
-      '</div>');
+      '</div>')
   }
 
   function add_card_armor(armor) {
     // Add a card to the deck named after the monster
     $('#card-deck').append('<div class="card"' +
       ' id=' + armor.id +
-      ' card-type="armor">');
+      ' card-type="armor">')
 
-    var armor_card = $('#' + armor.id);
+    var armor_card = $('#' + armor.id)
 
     // Top Label
     armor_card.append(
       '<div class="card-name">' +
       armor.name +
-      '</div>');
+      '</div>')
 
     // Image
     armor_card.append(
-      add_avatar(armor.image));
+      add_avatar(armor.image))
 
     // Stats
     armor_card.append(
-      '<div class="card-stats">');
+      '<div class="card-stats">')
 
     armor_card.find(".card-stats").append(
       add_stat("defense", armor.defense),
       add_stat("durability", armor.durability),
       add_stat("value", armor.value)
-    );
+    )
 
     // Description
     armor_card.append(
       '<div class="card-description">'+
       armor.description+
-      '</div>');
+      '</div>')
   }
 
   function add_card_weapon(weapon) {
     // Add a card to the deck named after the monster
     $('#card-deck').append('<div class="card"' +
       ' id=' + weapon.id +
-      ' card-type="armor">');
+      ' card-type="armor">')
 
-    var weapon_card = $('#' + weapon.id);
+    var weapon_card = $('#' + weapon.id)
 
     // Add background
     console.log(weapon.background);
     if (weapon.background) {
-      weapon_card.css('background-image',"url('"+weapon.background+"')");
+      weapon_card.css('background-image',"url('"+weapon.background+"')")
     }
 
     // Top Label
     weapon_card.append(
       '<div class="card-name">' +
       weapon.name +
-      '</div>');
+      '</div>')
 
     // Image
     weapon_card.append(
-      add_avatar(weapon.image));
+      add_avatar(weapon.image))
 
     // Stats
     weapon_card.append(
-      '<div class="card-stats">');
+      '<div class="card-stats">')
 
     weapon_card.find(".card-stats").append(
       add_stat("attack", weapon.attack),
       add_stat("durability", weapon.durability),
       add_stat("value", weapon.value)
-    );
+    )
 
     // Description
     weapon_card.append(
       '<div class="card-description">'+
       weapon.description+
-      '</div>');
+      '</div>')
   }
 
   function build_deck() {
     $('.card').each(function () {
-      $(this).remove();
-    });
+      $(this).remove()
+    })
 
     for (i in monsters) {
-      add_card_monster(monsters[i]);
+      add_card_monster(monsters[i])
     }
     for (i in armor) {
-      add_card_armor(armor[i]);
+      add_card_armor(armor[i])
     }
     for (i in weapons) {
-      add_card_weapon(weapons[i]);
+      add_card_weapon(weapons[i])
     }
   }
 
   function get_card_action(card, swipe_right) {
-    var card_type = $("#card-deck .card").first().attr('card-type');
+    var card_type = $("#card-deck .card")
+                      .first()
+                      .attr('card-type')
 
     if(swipe_right){
       switch(card_type) {
         case "monster":
-          return 'fight';
+          return 'fight'
         default:
           return 'run';
       }
     }else{
       switch(card_type) {
         case "monster":
-          return 'keep';
+          return 'keep'
         default:
-          return 'loot';
+          return 'loot'
       }
     }
   }
